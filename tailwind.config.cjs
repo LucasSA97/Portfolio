0f3a5dea69d8161893258c59ae9b18b0bf72fcbe
Lucas <lucassarachu7@gmail.com>
2023-12-17 20:10:41 +0100

    Agregando p sobre mi, boton para descargar cv y estilo en la foto mia

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,11 @@ module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				'xl-custom': '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+			  },
+		},
 	},
 	plugins: [],
 }
